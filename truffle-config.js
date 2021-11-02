1e82dcc80b40efc9236d3403aca815fe164f1445
Max <leonboripgs0818@outlook.com>
2021-11-02 11:38:57 +0900

    update config

--- truffle-config.js
+++ truffle-config.js
@@ -33,8 +33,8 @@ module.exports = {
       version: "0.8.7", 
       settings: {         
         optimizer: {
-          enabled: false,
-          runs: 200
+          enabled: true,
+          runs: 1500
         },
       }
     }
